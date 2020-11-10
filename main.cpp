4f68521e61c28296649533b10cf2b4fc5c4f91d7
Felix Braun <hazzl@falix.de>
2020-11-10 21:04:25 +0100

    fix singals and slots

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     Decoder d;
 
     d.setSourceFilename("/srv/daten/Musik/xiaoxingxing.ogg");
-    a.connect(&d, SIGNAL(Decoder::error(QAudioDecoder::Error e)), &a, SLOT(QCoreApplication::quit()));
-    a.connect(&d, SIGNAL(Decoder::finished()), &a, SLOT(QCoreApplication::quit()));
+    a.connect(&d, SIGNAL(error(QAudioDecoder::Error)), &a, SLOT(quit()));
+    a.connect(&d, SIGNAL(finished()), &a, SLOT(quit()));
     d.start();
 
     return a.exec();
